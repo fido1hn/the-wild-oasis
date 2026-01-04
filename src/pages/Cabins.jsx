cdee1657f10e2183db3899eaf0ebc3231a58355c
Confidence Ekeanya <confidoekeanya@gmail.com>
2026-01-04 02:53:46 +0100

    setting up storage buckets

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,16 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="http://127.0.0.1:54321/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        alt=""
+        srcset=""
+      />
     </Row>
   );
 }
